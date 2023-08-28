211b02838556a6b28a9020af59790938a5a3e5c3
UzSoftic <softuzb@gmail.com>
2023-08-28 17:51:17 +0500

    update router

--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Uzsoftic\LaravelEskiz\LaravelEskiz;
+use Uzsoftic\LaravelEskiz\LaravelEskizController;
 
 Route::name('eskizsms.')->prefix(config('laravel-eskiz.route_prefix'))->group(function (){
     Route::post('/send', [LaravelEskiz::class, 'send'])->name('send');
 });
 
 Route::name('eskizsms.')->prefix(config('laravel-eskiz.admin_route_prefix'))->middleware(config('laravel-eskiz.admin_middleware'))->group(function (){
-    Route::any('/dashboard', [LaravelEskiz::class, 'dashboard'])->name('dashboard');
-    Route::any('/listing', [LaravelEskiz::class, 'listing'])->name('listing');
-    Route::any('/config', [LaravelEskiz::class, 'config'])->name('config');
-    Route::any('/sender', [LaravelEskiz::class, 'sender'])->name('sender');
+    Route::any('/dashboard', [LaravelEskizController::class, 'dashboard'])->name('dashboard');
+    Route::any('/listing', [LaravelEskizController::class, 'listing'])->name('listing');
+    Route::any('/config', [LaravelEskizController::class, 'config'])->name('config');
+    Route::any('/sender', [LaravelEskizController::class, 'sender'])->name('sender');
 });
 
